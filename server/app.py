e815b2643af3ae02e5824f8a9d154e0e3c549992
mmwandera <mmwandera74@gmail.com>
2024-03-11 12:30:31 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -7,20 +7,170 @@ from sqlalchemy.exc import IntegrityError
 from config import app, db, api
 from models import User, Recipe
 
+# If the user's username and password are not authenticated: Return a JSON response with an error message, and a status of 401 (Unauthorized).
+# If the user is not logged in when they make the request: Return a JSON response with an error message, and a status of 401 (Unauthorized).
+
+@app.before_request
+def check_if_logged_in():
+    open_access_list = [
+        'signup',
+        'login',
+        'check_session'
+    ]
+
+    if (request.endpoint) not in open_access_list and (not session.get('user_id')):
+        return {'error': '401 Unauthorized'}, 401
+
+
+# SIGN UP FEATURE:
+# Handle sign up by implementing a POST /signup route. It should:
 class Signup(Resource):
-    pass
+    
+    # Be handled in a Signup resource with a post() method.
+    # In the post() method, if the user is valid:
+    def post(self):
 
+        # In the post() method, if the user is valid:
+        request_json = request.get_json()
+
+        username = request_json.get('username')
+        password = request_json.get('password')
+        image_url = request_json.get('image_url')
+        bio = request_json.get('bio')
+
+        # Save a new user to the database with their username, encrypted password, image URL, and bio.
+        user = User(
+            username=username,
+            image_url=image_url,
+            bio=bio
+        )
+
+        # the setter will encrypt this
+        user.password_hash = password
+
+        try:
+
+            db.session.add(user)
+            db.session.commit()
+
+            # Save the user's ID in the session object as user_id.
+            session['user_id'] = user.id
+
+            # Return a JSON response with the user's ID, username, image URL, and bio; and an HTTP status code of 201 (Created).
+            return user.to_dict(), 201
+
+        # If the user is not valid: Return a JSON response with the error message, and an HTTP status code of 422 (Unprocessable Entity).
+        except IntegrityError:
+
+            return {'error': '422 Unprocessable Entity'}, 422
+
+
+# AUTO-LOGIN FEATURE:
+# Handle auto-login by implementing a GET /check_session route. It should:
 class CheckSession(Resource):
-    pass
 
+    # Be handled in a CheckSession resource with a get() method.
+    def get(self):
+        
+        # In the get() method, if the user is logged in (if their user_id is in the session object):
+        user_id = session['user_id']
+        if user_id:
+            user = User.query.filter(User.id == user_id).first()
+            # Return a JSON response with the user's ID, username, image URL, and bio; and an HTTP status code of 200 (Success).
+            return user.to_dict(), 200
+        
+        # If the user is not logged in when they make the request: Return a JSON response with an error message, and a status of 401 (Unauthorized).
+        return {}, 401
+
+
+# LOGIN FEATURE:
+# Handle login by implementing a POST /login route. It should:
 class Login(Resource):
-    pass
+    
+    # Be handled in a Login resource with a post() method.
+    def post(self):
 
+        request_json = request.get_json()
+
+        username = request_json.get('username')
+        password = request_json.get('password')
+
+        user = User.query.filter(User.username == username).first()
+
+        # In the post() method, if the user's username and password are authenticated:
+        if user:
+            if user.authenticate(password):
+
+                # Save the user's ID in the session object.
+                session['user_id'] = user.id
+                # Return a JSON response with the user's ID, username, image URL, and bio.
+                return user.to_dict(), 200
+
+        # If the user's username and password are not authenticated: Return a JSON response with an error message, and a status of 401 (Unauthorized).  
+        return {'error': '401 Unauthorized'}, 401
+
+
+# LOGOUT FEATURE:
+# Handle logout by implementing a DELETE /logout route. It should:
 class Logout(Resource):
-    pass
 
+    # Be handled in a Logout resource with a delete() method.
+    # In the delete() method, if the user is logged in (if their user_id is in the session object):
+    def delete(self):
+
+        # Remove the user's ID from the session object.
+        session['user_id'] = None
+        # Return an empty response with an HTTP status code of 204 (No Content).
+        return {}, 204
+        
+
+
+# RECIPES FEATURE:
+# Handle recipe viewing by implementing a GET /recipes route. It should:
 class RecipeIndex(Resource):
-    pass
+
+    # RECIPE LIST FEATURE:
+    # Be handled in a RecipeIndex resource with a get() method
+    def get(self):
+
+        # In the get() method, if the user is logged in (if their user_id is in the session object):
+        user = User.query.filter(User.id == session['user_id']).first()
+        # Return a JSON response with an array of all recipes with their title, instructions, and minutes to complete data along with a nested user object; and an HTTP status code of 200 (Success).
+        return [recipe.to_dict() for recipe in user.recipes], 200
+        
+    # RECIPE CREATION FEATURE:
+    # Handle recipe creation by implementing a POST /recipes route. It should: 
+    # Be handled in the RecipeIndex resource with a post() method.
+    def post(self):
+
+        request_json = request.get_json()
+
+        title = request_json['title']
+        instructions = request_json['instructions']
+        minutes_to_complete = request_json['minutes_to_complete']
+
+        # In the post() method, if the user is logged in (if their user_id is in the session object):
+        try:
+
+            recipe = Recipe(
+                title=title,
+                instructions=instructions,
+                minutes_to_complete=minutes_to_complete,
+                user_id=session['user_id'],
+            )
+
+            # Save a new recipe to the database if it is valid. The recipe should belong to the logged in user, and should have title, instructions, and minutes to complete data provided from the request JSON.
+            db.session.add(recipe)
+            db.session.commit()
+
+            # Return a JSON response with the title, instructions, and minutes to complete data along with a nested user object; and an HTTP status code of 201 (Created).
+            return recipe.to_dict(), 201
+
+        # If the recipe is not valid:
+        except IntegrityError:
+            # Return a JSON response with the error messages, and an HTTP status code of 422 (Unprocessable Entity).
+            return {'error': '422 Unprocessable Entity'}, 422
+
 
 api.add_resource(Signup, '/signup', endpoint='signup')
 api.add_resource(CheckSession, '/check_session', endpoint='check_session')
--- server/models.py
+++ server/models.py
@@ -3,12 +3,64 @@ from sqlalchemy_serializer import SerializerMixin
 
 from config import db, bcrypt
 
+# Create a User model with the following attributes:
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
-    pass
+    serialize_rules = ('-recipes.user', '-_password_hash',)
 
+    id = db.Column(db.Integer, primary_key=True)
+    # username that is a String type.
+    # validate the user's username to ensure that it is present and unique (no two users can have the same username).
+    username = db.Column(db.String, unique=True, nullable=False)
+    # _password_hash that is a String type.
+    _password_hash = db.Column(db.String)
+    # image_url that is a String type.
+    image_url = db.Column(db.String)
+    # bio that is a String type.
+    bio = db.Column(db.String)
+
+    # have many recipes.
+    recipes = db.relationship('Recipe', backref='user')
+
+    # incorporate bcrypt to create a secure password. Attempts to access the password_hash should be met with an AttributeError.
+    # A hybrid property is a feature provided by the SQLAlchemy library in Python. It allows a method to be accessed as an attribute, providing both the behavior of a property and a method. This means it can be used like an attribute, but it can also contain custom logic like a method.
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError('Password hashes may not be viewed.')
+
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8'))
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+# Next, create a Recipe model with the following attributes:
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
+    __table_args__ = (
+        db.CheckConstraint('length(instructions) >= 50'),
+    )
+
+    id = db.Column(db.Integer, primary_key=True)
+    # title that is a String type.
+    # title must be present.
+    title = db.Column(db.String, nullable=False)
+    # instructions that is a String type.
+    # instructions must be present and at least 50 characters long.
+    instructions = db.Column(db.String, nullable=False)
+    # minutes_to_complete that is an Integer type.
+    minutes_to_complete = db.Column(db.Integer)
     
-    pass
+    # a recipe belongs to a user.
+    user_id = db.Column(db.Integer(), db.ForeignKey('users.id'))
+
+    def __repr__(self):
+        return f'<Recipe {self.id}: {self.title}>'
